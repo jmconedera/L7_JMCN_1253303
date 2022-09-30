d2574ff0361d780ba4517f430da299b9272b340c
MCONEDERA <MCONEDERA@GLC2539>
2022-09-30 13:28:31 -0600

    Add project files.

---
+++ L7_JMCN_1253303/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace L7_JMCN_1253303
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(337, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese un número mayor a cero:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(552, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Serie de Fibonacci:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(265, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Labortorio No.7_1253303";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ L7_JMCN_1253303/Form1.cs
@@ -0,0 +1,50 @@
+namespace L7_JMCN_1253303
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int N = int.Parse(textBox1.Text);
+            int A = 0;
+            int B = 1;
+            int C = 0;
+            int i = 2;
+            string resultado = "";
+            if(N > 0)
+            {
+                resultado = resultado + Convert.ToString(A)+", ";
+                if (N>1)
+                {
+                    resultado = resultado + Convert.ToString(B) + ", ";
+                    while (i< N)
+                    {
+                        C = A + B;
+                        resultado = resultado + Convert.ToString(C) + ", ";
+                        A = B;
+                        B = C;
+                        i = i + 1;
+                    }
+                    label3.Text = Convert.ToString(resultado);
+                }
+                else
+                {
+                    label3.Text = Convert.ToString(resultado);
+                }
+            }
+            else
+            {
+                label3.Text = Convert.ToString(resultado);
+            }
+        }
+    }
+}
